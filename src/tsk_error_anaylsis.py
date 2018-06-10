432d4c853aa805a5c691d628b98f763fc8866552
zhengjian518 <zhengjian518@outlook.com>
2018-06-10 21:40:51 +0200

    write_task_result_to_csv()

--- src/tsk_error_anaylsis.py
+++ src/tsk_error_anaylsis.py
@@ -19,32 +19,34 @@ import operator
 import csv
 
 def task_err_analusis_all_patients(acc_result_path,tfd_result_path,task,window_size):
-
+	"""
+	start from all patients
+	"""
 	# step 1
 	# read in the results
 	acc_full_path = util.get_full_path_under_folder(acc_result_path)
 	acc_full_path = sorted(acc_full_path,key=lambda x: (int(re.sub('\D','',x)),x))
-	task_num = len(acc_full_path)
+	code_num = len(acc_full_path)
 	acc_result = []
 	phase_result = []
 	rgb_result = []
 	code_list = []
 	peak_mark = []
-	for task_idx in range(0,task_num):
-		task_code = acc_full_path[task_idx].split('/')[5]
-		if not os.path.isdir(acc_full_path[task_idx] + '{}/'.format(task)) \
-			or not os.path.isdir(tfd_result_path + '{}_joint_tfd_{}/'.format(task,window_size) +task_code + '_tfd/'):
+	for code_idx in range(0,code_num):
+		patient_code = acc_full_path[code_idx].split('/')[5]
+		if not os.path.isdir(acc_full_path[code_idx] + '{}/'.format(task)) \
+			or not os.path.isdir(tfd_result_path + '{}_joint_tfd_{}/'.format(task,window_size) +patient_code + '_tfd/'):
 			continue # no such task in this patient folder
 		
-		accfreq_txt_path = acc_full_path[task_idx] + '{}/freq.txt'.format(task)
+		accfreq_txt_path = acc_full_path[code_idx] + '{}/freq.txt'.format(task)
 		all_acclines = np.loadtxt(accfreq_txt_path)
 		
 		if not int(all_acclines[-1,:][0]) == 0: # periodic detected
-			code = task_code.split('_')[0]
+			code = patient_code.split('_')[0]
 			code_list.append(code)
 			acc_result.append(list(all_acclines[-1,:])) # 2 columns
-			peak_mark.append(task_idx)
-			tfd_folder = tfd_result_path + '{}_joint_tfd_{}/'.format(task,window_size) +task_code + '_tfd/'+ 'freq_psd_txt/'
+			peak_mark.append(code_idx)
+			tfd_folder = tfd_result_path + '{}_joint_tfd_{}/'.format(task,window_size) +patient_code + '_tfd/'+ 'freq_psd_txt/'
 			phasefreq_txt_path = tfd_folder + 'freq_result.txt'
 			all_phaselines = np.loadtxt(phasefreq_txt_path)
 			phase_result.append(list(all_phaselines[-1,:])) # 3 columns
@@ -122,21 +124,66 @@ def write_baseline_to_csv(acc_result_path):
 			csvwriter.writerow(row)
 		del csvwriter
 
+def write_tfd_to_csv(tfd_result_path,acc_result_path,window_size):
+	"""
+	start from every task
+	"""
+	tasks_path = util.get_full_path_under_folder(tfd_result_path)
+	tasks_path = sorted(tasks_path,key=lambda x: (int(re.sub('\D','',x)),x))
+	csv_folder = '/local/guest/joint_postion/tremor-freq-detection/error_analysis/CSVs/'
+	if not os.path.isdir(csv_folder):
+		os.makedirs(csv_folder)
 
+	# get all patient_codes
+	patient_codes = []
+	acc_full_path = util.get_full_path_under_folder(acc_result_path)
+	acc_full_path = sorted(acc_full_path,key=lambda x: (int(re.sub('\D','',x)),x))
+	for patient_code_path in acc_full_path:
+		code = patient_code_path.split('/')[5]
+		patient_codes.append(code)
+
+	for task_path in tasks_path:
+		task_name = task_path.split('/')[6]
+		task_name = re.sub('_joint_tfd_61','',task_name)
+		csv_save_path = csv_folder + '{}.csv'.format(task_name)
+		csvfile = open(csv_save_path, 'wb')
+		csvwriter = csv.writer(csvfile)
+		csvwriter_head = ['Patient_code','Freq_acc','Freq_phase','Freq_rgb','IsPeak_acc','IsPeak_phase','IsPeak_rgb']
+		csvwriter.writerow(csvwriter_head)
+
+		for code in patient_codes:
+			acc_path = acc_result_path + code + '/freq.txt'
+			if not os.path.isfile(acc_path):
+				continue
+
+			tfd_folder = tfd_result_path + '{}_joint_tfd_{}/'.format(task_name,window_size) +code + '_tfd/'+ 'freq_psd_txt/'
+			phasefreq_txt_path = tfd_folder + 'freq_result.txt'
+			all_phaselines = np.loadtxt(phasefreq_txt_path)
+
+			rgbfreq_txt_path = tfd_folder + 'freq_rgb.txt'
+			all_rgblines = np.loadtxt(rgbfreq_txt_path)
+
+			all_acclines = np.loadtxt(acc_path)
+			# row = ['Patient_code','Freq_acc','Freq_phase','Freq_rgb','IsPeak_acc','IsPeak_phase','IsPeak_rgb']
+			row = [code,all_acclines[-1,:][1],all_phaselines[-1,:][1],all_rgblines[-1,:][1],\
+					all_acclines[-1,:][0],all_phaselines[-1,:][0],all_rgblines[-1,:][0]]
+			csvwriter.writerow(row)
+		del csvwriter
 
 if __name__ == "__main__":
 
 	acc_result_path = '/local/guest/benchmark_acc/baseline_win61/'
-	# tfd_result_path = '/local/guest/joint_postion/tremor-freq-detection/result/'
+	tfd_result_path = '/local/guest/joint_postion/tremor-freq-detection/result/'
 	# # task = '100-7'
 	# # task = 'Rust'
 	# task_list = ['100-7','2_hz_lager','2_hz_hoger','Duimen_omhoog','Fingertap','Handen_in_pronatie',\
 	# 			'Maanden_terug','Pianospelen','Rust','Schrijven_links','Schrijven_rechts',\
 	# 			'Spiraal_links','Spiraal_rechts','Top_neus_links','Top_neus_rechts','Top-top','Volgen']
-	# window_size = '61'
+	window_size = '61'
 	# for task in task_list:
 	# 	print 'error analysis on task {}'.format(task)
 	# 	task_err_analusis_all_patients(acc_result_path,tfd_result_path,task,window_size)
 
-	# acc_result_path = '/home/jianz/OnCluster/baseline_win61/'
-	write_baseline_to_csv(acc_result_path)
+	# write_baseline_to_csv(acc_result_path)
+
+	write_tfd_to_csv(tfd_result_path,acc_result_path,window_size):

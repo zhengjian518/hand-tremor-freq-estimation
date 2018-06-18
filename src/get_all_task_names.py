1eabe65640816cd42c7c4d63f3e17c5bc807518d
zhengjian518 <zhengjian518@outlook.com>
2018-06-18 19:46:38 +0200

    unicode

--- src/get_all_task_names.py
+++ src/get_all_task_names.py
@@ -1,17 +1,20 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 import os 
 import util
-
+import re
 all_tasks_name = []
 path = '/media/tremor-data/TremorData_split/Tremor_data/'
 
 all_patient_code_path = util.get_full_path_under_folder(path)
 
 for i in range(0,len(all_patient_code_path)):
-	task_names = util.get_dir_list(all_patient_code_path[i])
-	all_tasks_name = all_tasks_name + task_names
-
+        task_names = util.get_dir_list(all_patient_code_path[i])
+        all_tasks_name = all_tasks_name + task_names
+        print all_patient_code_path[i]
 all_name = set(all_tasks_name)
-name_list = sorted(all_name,key=lambda x: (int(re.sub('\D','',x)),x))
+for all_task in all_name:
 
-print name_list
+sorted(all_name)
+print all_name
+print len(all_name)
